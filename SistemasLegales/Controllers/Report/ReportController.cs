8373b94101ddbdb3d88830bce4c96346cf1c9311
Néstor Yasmani Valle Rabelo <nestorvalle880416@gmail.com>
2020-11-18 22:26:40 -0500

    Se adiciona el parámetro empresa en los reportes , es configurable el nombre en el AppSetting (NombreParametroIdEmpresa)

--- SistemasLegales/Controllers/Report/ReportController.cs
+++ SistemasLegales/Controllers/Report/ReportController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using SistemasLegales.Models.Entidades;
 using SistemasLegales.Services;
+using System;
 
 namespace SistemasLegales.Controllers
 {
@@ -8,24 +11,32 @@ namespace SistemasLegales.Controllers
     {
 
         private readonly IReporteServicio reporteServicio;
+        private readonly UserManager<ApplicationUser> _userManager;
         public IConfiguration Configuration { get; }
-        public ReportController( IReporteServicio reporteServicio, IConfiguration Configuration)
+        public ReportController( IReporteServicio reporteServicio, IConfiguration Configuration, UserManager<ApplicationUser> userManager)
         {
             this.Configuration = Configuration;
             this.reporteServicio = reporteServicio;
+            _userManager = userManager;
         }
 
       
         public ActionResult RepTramites(int id)
         {
+            var UsuarioAutenticado = _userManager.GetUserAsync(User).Result;
+            var nombreParametroIdEmpresa = Configuration.GetSection("NombreParametroIdEmpresa").Value;
             var parametersToAdd = reporteServicio.GetDefaultParameters(Configuration.GetSection("ReporteTramites").Value);
+            parametersToAdd = reporteServicio.AddParameters(nombreParametroIdEmpresa,Convert.ToString(UsuarioAutenticado.IdEmpresa),parametersToAdd);
             var newUri = reporteServicio.GenerateUri(parametersToAdd);
             return Redirect(newUri);
         }
 
         public ActionResult ReporteEstadoVsTiempo()
         {
+            var UsuarioAutenticado = _userManager.GetUserAsync(User).Result;
+            var nombreParametroIdEmpresa = Configuration.GetSection("NombreParametroIdEmpresa").Value;
             var parametersToAdd = reporteServicio.GetDefaultParameters(Configuration.GetSection("ReporteEstadoVsTiempo").Value);
+            parametersToAdd = reporteServicio.AddParameters(nombreParametroIdEmpresa, Convert.ToString(UsuarioAutenticado.IdEmpresa), parametersToAdd);
             var newUri = reporteServicio.GenerateUri(parametersToAdd);
             return Redirect(newUri);
         }
